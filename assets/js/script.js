43fd2972632f0a5ca42116e8c8944d3ca79c1efa
erickpetterson <epettersondossantos@gmail.com>
2022-12-10 18:10:05 -0400

    atualização

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// botao de alterar quantidade
+
 const myInput = document.getElementById("my-input");
 function stepper(btn) {
     let id = btn.getAttribute("id");
@@ -5,7 +7,7 @@ function stepper(btn) {
     let max = myInput.getAttribute("max");
     let step = myInput.getAttribute("step");
     let val = myInput.getAttribute("value");
-    let calcStep = (id == "increment") ? (step*1) :
+    let calcStep = (id == "increment") ? (step * 1) :
         (step * -1);
     let newValue = parseInt(val) + calcStep;
 
@@ -14,3 +16,4 @@ function stepper(btn) {
     }
 
 }
+
