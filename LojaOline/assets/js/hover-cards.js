3560c4dd8738e1f33fa8ddf7ed631d4d83ed1874
Erick Petterson <106610473+erickpetterson@users.noreply.github.com>
2022-12-18 12:56:30 -0400

    Add files via upload

---
+++ LojaOline/assets/js/hover-cards.js
@@ -0,0 +1,73 @@
+// card hover
+
+var card = document.querySelector(".card1")
+
+card.addEventListener("mouseover", function () {
+
+    var bottom = document.querySelector("#bottom-hover1");
+
+    bottom.classList.remove("d-none");
+
+});
+
+card.addEventListener("mouseout", function () {
+
+    var bottom = document.querySelector("#bottom-hover1");
+
+    bottom.classList.add("d-none");
+
+});
+
+var card = document.querySelector(".card2")
+
+card.addEventListener("mouseover", function () {
+
+    var bottom = document.querySelector("#bottom-hover2");
+
+    bottom.classList.remove("d-none");
+
+});
+
+card.addEventListener("mouseout", function () {
+
+    var bottom = document.querySelector("#bottom-hover2");
+
+    bottom.classList.add("d-none");
+
+});
+
+var card = document.querySelector(".card3")
+
+card.addEventListener("mouseover", function () {
+
+    var bottom = document.querySelector("#bottom-hover3");
+
+    bottom.classList.remove("d-none");
+
+});
+
+card.addEventListener("mouseout", function () {
+
+    var bottom = document.querySelector("#bottom-hover3");
+
+    bottom.classList.add("d-none");
+
+});
+
+var card = document.querySelector(".card4")
+
+card.addEventListener("mouseover", function () {
+
+    var bottom = document.querySelector("#bottom-hover4");
+
+    bottom.classList.remove("d-none");
+
+});
+
+card.addEventListener("mouseout", function () {
+
+    var bottom = document.querySelector("#bottom-hover4");
+
+    bottom.classList.add("d-none");
+
+});
